f4fccae26f48a17966e0e67f730edae25683f6d3
Isaac <isaac.yeo.linux@gmail.com>
2019-12-18 22:00:17 +0800

    Added test for memory efficiency and speed of llist

---
+++ tests/llist_huge.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define CUSTOM_GT /* Must come before inclusion of types.h! */
+typedef union {
+	int int_t;
+} generic_t;
+#include "../types.h"
+#include "../llist/headers/llist.h"
+
+
+int main(void)
+{
+	struct llist *new = llist_create();
+
+	for(int i = 0; i < 1000000; i++)
+		llist_addnode(new, TO_GT i, i);
+
+	/*
+	for (int i = 0; i < new->len; i++)
+		printf("%d\n", llist_getval(new, i).int_t);
+	*/
+
+	printf("Memory used: %d bytes | Constant size %d + %d nodes of size %d each\n", \
+			new->len * sizeof(struct llist_node) + sizeof(struct llist), \
+			sizeof(struct llist), new->len, sizeof(struct llist_node));
+
+	llist_del(new);
+}
